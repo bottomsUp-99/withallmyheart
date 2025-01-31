a0504cd3818b61cbc637453541d82ac0afcbc91a
leejuhwan <seungjoon0324@gmail.com>
2025-01-31 18:02:33 +0900

    백준 입출력과 사칙연산 완료

---
+++ src/백준/baekjoon2557.java
@@ -0,0 +1,7 @@
+package 백준;
+
+public class baekjoon2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/백준/baekjoon2588.java
@@ -0,0 +1,29 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekjoon2588 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int first = Integer.parseInt(br.readLine());
+        int second = Integer.parseInt(br.readLine());
+        int third = 0, fourth = 0, fifth = 0;
+        for (int i = 0 ; i < 3 ; i++){
+            int share = second%10;
+            if (i == 0){
+                third = share * first;
+            } else if(i == 1){
+                fourth = share * first;
+            } else {
+                fifth = share * first;
+            }
+            second /= 10;
+        }
+        System.out.println(third);
+        System.out.println(fourth);
+        System.out.println(fifth);
+        System.out.println(third + fourth * 10 + fifth * 100);
+    }
+}
