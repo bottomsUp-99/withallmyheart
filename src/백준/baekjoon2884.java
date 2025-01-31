dd49c858c2b68e0d66c9ed7afe68865804b07803
leejuhwan <seungjoon0324@gmail.com>
2025-01-31 18:56:49 +0900

    백준 조건문 완료

---
+++ src/백준/baekjoon2884.java
@@ -0,0 +1,24 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon2884 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        if (m < 45){
+            if (h == 0){
+                System.out.println(23 + " " + (60 + m - 45));
+            } else {
+                System.out.println((h - 1) + " " + (60 + m - 45));
+            }
+        } else {
+            System.out.println(h + " " + (m - 45));
+        }
+    }
+}
---
+++ src/백준/baekjoon9498.java
@@ -0,0 +1,23 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekjoon9498 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int score = Integer.parseInt(br.readLine());
+        if (score < 60){
+            System.out.println("F");
+        } else if (score < 70) {
+            System.out.println("D");
+        } else if (score < 80) {
+            System.out.println("C");
+        } else if (score < 90){
+            System.out.println("B");
+        } else {
+            System.out.println("A");
+        }
+    }
+}
