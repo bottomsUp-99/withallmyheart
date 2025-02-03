83fd2a524c4561fca2d13c615440cd7d76ff6527
leejuhwan <seungjoon0324@gmail.com>
2025-02-03 15:15:33 +0900

    백준 배열 5번째까지 완료

---
+++ src/백준/baekjoon10818.java
@@ -0,0 +1,21 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon10818 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int compare = Integer.parseInt(st.nextToken());
+            min = Math.min(min, compare);
+            max = Math.max(max, compare);
+        }
+        System.out.println(min + " " + max);
+    }
+}
