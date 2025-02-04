9fd5c537baeb3ecbcb123b455d612f4b9db8bc0d
leejuhwan <seungjoon0324@gmail.com>
2025-02-04 14:26:30 +0900

    백준 1차원 배열 완료

---
+++ src/백준/baekjoon10811.java
@@ -0,0 +1,34 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon10811 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] basket = new int[n];
+        for (int i = 0; i < basket.length; i++) {
+            basket[i] = i + 1;
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int lt = Integer.parseInt(st.nextToken());
+            int rt = Integer.parseInt(st.nextToken());
+            while (lt < rt){
+                int tmp = basket[lt - 1];
+                basket[lt - 1] = basket[rt - 1];
+                basket[rt - 1] = tmp;
+                lt++;
+                rt--;
+            }
+        }
+        for (int a : basket){
+            System.out.print(a + " ");
+        }
+    }
+}
---
+++ src/백준/baekjoon3052.java
@@ -0,0 +1,18 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class baekjoon3052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashSet<Integer> rest = new HashSet<>();
+        for (int i = 0; i < 10; i++) {
+            int num = Integer.parseInt(br.readLine());
+            rest.add(num % 42);
+        }
+        System.out.println(rest.size());
+    }
+}
