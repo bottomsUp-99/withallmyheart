551de81054c106929daa4f2e8b7ea9a5892d1a40
leejuhwan <seungjoon0324@gmail.com>
2025-02-09 12:43:33 +0900

    인프런 소수 문제 솔루션 추가

--- src/인프런/section3/section3_5.java
+++ src/인프런/section3/section3_5.java
@@ -7,33 +7,48 @@ import java.io.InputStreamReader;
 public class section3_5 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); // 자연수 N 입력 받기
-        System.out.println(countPrimes(n)); // 소수 개수 출력
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(countPrimes_1(n));
+        System.out.println(countPrimes_2(n));
     }
 
-    public static int countPrimes(int n) {
-        if (n < 2) return 0; // 1 이하는 소수가 아님
+    public static int countPrimes_1(int n) {
+        if (n < 2) return 0;
 
-        boolean[] isPrime = new boolean[n + 1]; // 소수 여부 저장 배열
-        int count = 0; // 소수 개수
+        boolean[] isPrime = new boolean[n + 1];
+        int count = 0;
 
-        // 모든 수를 소수(true)로 초기화
         for (int i = 2; i <= n; i++) {
             isPrime[i] = true;
         }
 
-        // 에라토스테네스의 체 알고리즘 적용
         for (int i = 2; i * i <= n; i++) {
-            if (isPrime[i]) { // i가 소수이면
+            if (isPrime[i]) {
                 for (int j = i * i; j <= n; j += i) {
-                    isPrime[j] = false; // i의 배수는 소수가 아님
+                    isPrime[j] = false;
                 }
             }
         }
 
-        // 소수 개수 세기
         for (int i = 2; i <= n; i++) {
             if (isPrime[i]) count++;
+        }
+        return count;
+    }
+
+    public static int countPrimes_2(int n){
+        int[] isPrime = new int[n + 1];
+        int count = 0;
+        for (int i = 1; i < isPrime.length; i++) {
+            isPrime[i] = 0;
+        }
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i] == 0){
+                count++;
+                for (int j = i; j <= n; j+= i) {
+                    isPrime[j] = 1;
+                }
+            }
         }
         return count;
     }
