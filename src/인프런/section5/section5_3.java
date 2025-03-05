745bc8001cc2bf623115dc1de1de95c0ec373c4f
leejuhwan <seungjoon0324@gmail.com>
2025-03-05 15:38:03 +0900

    섹션 5-3 시간초과 문제 해결 완료

--- src/인프런/section5/section5_3.java
+++ src/인프런/section5/section5_3.java
@@ -3,12 +3,32 @@ package 인프런.section5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class section5_3 {
-    public static void main(String[] args) throws IOException {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int n = Integer.parseInt(st.nextToken());
+//        int k = Integer.parseInt(st.nextToken());
+//        st = new StringTokenizer(br.readLine());
+//        int[] num = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            num[i] = Integer.parseInt(st.nextToken());
+//        }
+//        int lt = 0;
+//        for (int rt = k - 1; rt < n; rt++) {
+//            HashSet<Integer> profit = new HashSet<>();
+//            for (int j = lt; j <= rt ; j++) {
+//                profit.add(num[j]);
+//            }
+//            sb.append(profit.size()).append(" ");
+//            lt++;
+//        }
+//        System.out.println(sb);
+//    }
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -19,15 +39,22 @@ public class section5_3 {
         for (int i = 0; i < n; i++) {
             num[i] = Integer.parseInt(st.nextToken());
         }
-        int lt = 0;
-        for (int rt = k - 1; rt < n; rt++) {
-            HashSet<Integer> profit = new HashSet<>();
-            for (int j = lt; j <= rt ; j++) {
-                profit.add(num[j]);
-            }
-            sb.append(profit.size()).append(" ");
+        for(int x : solution(n, k, num)) System.out.print(x+" ");
+    }
+    public static ArrayList<Integer> solution(int n, int k, int[] arr){
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<Integer, Integer> HM = new HashMap<>();
+        for(int i=0; i<k-1; i++){
+            HM.put(arr[i], HM.getOrDefault(arr[i], 0)+1);
+        }
+        int lt=0;
+        for(int rt=k-1; rt<n; rt++){
+            HM.put(arr[rt], HM.getOrDefault(arr[rt], 0)+1);
+            answer.add(HM.size());
+            HM.put(arr[lt], HM.get(arr[lt])-1);
+            if(HM.get(arr[lt])==0) HM.remove(arr[lt]);
             lt++;
         }
-        System.out.println(sb);
+        return answer;
     }
 }
